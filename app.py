d3acc037dce05a62d6a085f6b4a44e36a8b41dc5
peter nadel <peter.m.nadel@gmail.com>
2023-11-27 15:35:46 -0500

    aws db connection

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import utils
-import os
 
 css_cdn = """<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">"""
 js_cdn  = """<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>"""
@@ -8,17 +7,12 @@ js_cdn  = """<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/b
 st.markdown(css_cdn, unsafe_allow_html=True)
 st.markdown(js_cdn, unsafe_allow_html=True)
 st.markdown(utils.css,unsafe_allow_html=True)
-
-utils.prep_data()
-em = utils.prep_embeddings()
-db = utils.prep_db(em)
-
 st.markdown('<h1>Flint FOIA Response Search Engine</h1>', unsafe_allow_html=True)
 st.markdown(f'<p style="text-align:center">{utils.MAIN_INFO}</p>', unsafe_allow_html=True)
-st.expander('Extra info TBD')
+# st.expander('Extra info TBD')
 
 search_bar = st.empty()
-user_query = search_bar.text_input("Search the emails", key=1) # st.text_input("Search the emails")
+user_query = search_bar.text_input("Search the emails", key=1)
 suggested = ['local engagement', 'state of emergency', 'water contamination', 'schools', 'lead pipes']
 with st.expander('Some suggested queries'):
     for s, col in zip(suggested, st.columns([.2, .2, .25, .15, .2])):
@@ -28,10 +22,16 @@ with st.expander('Some suggested queries'):
                 search_bar.text_input("Search the emails", key=2, value=s)
 query = utils.QUERY_PROMPT + user_query
 num_responses = st.number_input('Maximum number of documents to see', step=1, min_value=1, value=10)
-raw_responses = db.similarity_search(query, k=num_responses+25)
-valid_ids = utils.remove_duplicates([rr.page_content for rr in raw_responses])
-valid_responses = [raw_responses[i] for i in [vi[0] for vi in valid_ids]][:num_responses]
+
+model = utils.init_model()
+conn = utils.init_db()
+st.divider()
 
 if user_query != '':
+    query_embedding = model.encode(query)
+    raw_responses = conn.execute(utils.RERANK_SQL, {'query': query, 'embedding': query_embedding, 'k': utils.K, 'k_comp': utils.K_COMP}).fetchall()
+    # raw_responses = conn.execute(utils.SQL, {'query_embedding': query_embedding}).fetchall()
+    valid_ids = utils.remove_duplicates([rr[1] for rr in raw_responses])
+    valid_responses = [raw_responses[i] for i in [vi[0] for vi in valid_ids]][:num_responses]
     for vr in valid_responses:
         utils.display(vr)
--- utils.py
+++ utils.py
@@ -1,12 +1,30 @@
 import streamlit as st
-from langchain.embeddings import HuggingFaceBgeEmbeddings
-from langchain.vectorstores import FAISS
 from thefuzz import fuzz
-from ast import literal_eval
-import gdown
+from sentence_transformers import SentenceTransformer
+from pgvector.psycopg import register_vector
+import psycopg
+import requests
+import re
 
 QUERY_PROMPT  = 'Represent this sentence for searching relevant passages: '
-METADATA_KEYS = ['From', 'To', 'Cc', 'Date', 'Subject', 'Attachment']
+METADATA_KEYS = ['From', 'Date', 'To', 'Cc', 'Subject', 'Attachment']
+VALID_ARCHIVE = [
+    "https://archive.org/details/snyder_flint_emails/MSP004/",
+    "https://archive.org/details/snyder_flint_emails/MSP005/",
+    "https://archive.org/details/snyder_flint_emails/MSP008/",
+    "https://archive.org/details/snyder_flint_emails/MSP009/",
+    "https://archive.org/details/snyder_flint_emails/MSP011/",
+    "https://archive.org/details/snyder_flint_emails/MSP012/",
+    "https://archive.org/details/snyder_flint_emails/Staff_1/",
+    "https://archive.org/details/snyder_flint_emails/Staff_10/",
+    "https://archive.org/details/snyder_flint_emails/Staff_11/",
+    "https://archive.org/details/snyder_flint_emails/Staff_12/",
+    "https://archive.org/details/snyder_flint_emails/Staff_13/",
+    "https://archive.org/details/snyder_flint_emails/Staff_14/",
+    "https://archive.org/details/snyder_flint_emails/Staff_15/",
+    "https://archive.org/details/snyder_flint_emails/Staff_16/",
+    "https://archive.org/details/snyder_flint_emails/Staff_17/"
+]
 
 MAIN_INFO = """
 Peter Nadel and Kevin Smith, Tufts University
@@ -32,24 +50,63 @@ css = """
         </style>
         """
 
-@st.cache_data(ttl="1d")
-def prep_data():
-    flint_920_url = 'https://drive.google.com/drive/folders/1dXnLDBpVtfSo6SncSdOlidcAMnkbeiG8?usp=sharing'
-    gdown.download_folder(flint_920_url, use_cookies=False)
+RERANK_SQL = """
+WITH semantic_search AS (
+    SELECT id, uid, content, embedding, "image_lookup", "From", "Sent", "To", "Cc", "Subject", "Attachment", "thread_index", RANK () OVER (ORDER BY embedding <=> %(embedding)s) AS rank
+    FROM documents
+    ORDER BY embedding <=> %(embedding)s
+    LIMIT 200
+),
+keyword_search AS (
+    SELECT id, uid, content, RANK () OVER (ORDER BY ts_rank_cd(to_tsvector('english', content), query) DESC)
+    FROM documents, plainto_tsquery('english', %(query)s) query
+    WHERE to_tsvector('english', content) @@ query
+    ORDER BY ts_rank_cd(to_tsvector('english', content), query) DESC
+    LIMIT 150
+)
+SELECT
+    COALESCE(semantic_search.id, keyword_search.id) AS id,
+    semantic_search.uid,
+    semantic_search.content,
+    semantic_search."image_lookup",
+    semantic_search."From",
+    semantic_search."Sent",
+    semantic_search."To",
+    semantic_search."Cc",
+    semantic_search."Subject",
+    semantic_search."Attachment",
+    semantic_search."thread_index",
+    COALESCE(1.0 / (%(k)s + semantic_search.rank), 0.0) +
+    COALESCE(1.0 / (%(k_comp)s + keyword_search.rank), 0.0) AS score
+FROM semantic_search
+FULL OUTER JOIN keyword_search ON semantic_search.id = keyword_search.id
+ORDER BY score DESC
+LIMIT 100
+"""
+
+SQL = 'SELECT id, uid, content, "image_lookup", "From", "Sent", "To", "Cc", "Subject", "Attachment", "thread_index" FROM documents ORDER BY embedding <=> %(query_embedding)s LIMIT 50'
+
+K = 70
+K_COMP = 100-K
 
 @st.cache_resource
-def prep_embeddings():
-    model_norm = HuggingFaceBgeEmbeddings(
-        model_name="BAAI/bge-base-en",
-        model_kwargs={'device': 'cpu'},
-        encode_kwargs={'normalize_embeddings': True}
-    )
-    return model_norm
+def init_model():
+    return SentenceTransformer('BAAI/bge-large-en-v1.5')
 
-def prep_db(em):
-    return FAISS.load_local('./flint_920', em)
+def init_db():
+    conn = psycopg.connect(
+        dbname=st.secrets['manual_db_connection']['dbname'],
+        user=st.secrets['manual_db_connection']['user'], 
+        password=st.secrets['manual_db_connection']['password'],
+        host = st.secrets['manual_db_connection']['host'], 
+        port = st.secrets['manual_db_connection']['port'], 
+        autocommit=True
+        )
+    conn.execute('CREATE EXTENSION IF NOT EXISTS vector')
+    register_vector(conn)
+    return conn
 
-def remove_duplicates(strings, threshold=70):
+def remove_duplicates(strings, threshold=85):
     unique_strings = []
     for i, string in enumerate(strings):
         is_dup = False
@@ -67,7 +124,7 @@ def escape_markdown(text):
     MD_SPECIAL_CHARS = "\`*_{}#+$"
     for char in MD_SPECIAL_CHARS:
         text = text.replace(char, '').replace('\t', '')
-    return text
+    return text.replace('"', '')
 
 def format_image_path(text_name):
     directory = '_'.join(text_name.split('_')[:-1])
@@ -75,14 +132,14 @@ def format_image_path(text_name):
     return f"https://rjdgrlmrpwnzwmdwgizseanca.s3.us-east-2.amazonaws.com/{directory}_jp2/{fname}"
 
 def display(doc):
-    md = doc.metadata
+    md = {key:value.replace("{", "").replace("}", "") for key, value in zip(METADATA_KEYS, doc[4:10])}
     for key in METADATA_KEYS:
-        if md[key] != '[]':
-            meta_list = literal_eval(md[key])
+        if md[key] != '':
+            meta_list = md[key].split('","')
             st.markdown(f"<small style='text-align: right;'>{key}: <b>{', '.join([escape_markdown(m) for m in meta_list])}</b></small>",unsafe_allow_html=True)
-    text_names  = literal_eval(md['image_lookup'])
-    with st.expander('See the Parsed Email Text'):
-        st.markdown(doc.page_content)
+    text_names  = doc[3].replace('{', '').replace('}', '').split(',')
+    with st.expander('See the Parsed Text'):
+        st.markdown(doc[1])
     image_paths = [format_image_path(tn) for tn in text_names] 
     if len(image_paths) > 1:
         image_tabs = st.tabs([ip.split('/')[-1] for ip in image_paths])
@@ -90,4 +147,12 @@ def display(doc):
             tab.image(image_paths[i], caption=image_paths[i].split('/')[-1])
     else:
         st.image(image_paths[0], caption=image_paths[0].split('/')[-1])
+    repo = re.search(r"([^\/]+)_jp2", image_paths[0]).group(1)
+    non_pdf = f"https://archive.org/details/snyder_flint_emails/{repo}"
+    if non_pdf in VALID_ARCHIVE:
+        link = f'[Click here to explore more on Archive.org]({non_pdf})'
+    else:
+        pdf_link = f"https://archive.org/details/snyder_flint_emails/{repo}.pdf"
+        link = f'[Click here to explore more on Archive.org]({pdf_link})'
+    st.markdown(link)
     st.divider()
